3d95a62700a0959d5ccc24cc4cbc58bb31ffc45f
RizwanSalim <rizwansalimks1998@gmail.com>
2024-10-16 14:07:15 +0530

    ch1

--- SchoolManagementApp/Models/Student.cs
+++ SchoolManagementApp/Models/Student.cs
@@ -13,7 +13,6 @@ namespace SchoolManagementApp.Models
         public string Gender { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string Password { get; set; }
         public List<Qualification> Qualifications { get; set; } = new List<Qualification>();
     }
 }
